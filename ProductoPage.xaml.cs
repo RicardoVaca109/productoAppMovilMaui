3a261e1f0aa95bde5ad2b00e60a044acefb97896
Ricardo.Vaca <ricardo.vaca@udla.edu.ec>
2023-11-21 14:27:26 -0500

    Añadir Botón Eliminar

--- ProductoPage.xaml.cs
+++ ProductoPage.xaml.cs
@@ -2,8 +2,8 @@
 using Microsoft.Maui;
 using static System.Net.Mime.MediaTypeNames;
 using System.Threading;
-using CommunityToolkit.Maui.Core;
-using CommunityToolkit.Maui.Alerts;
+//using CommunityToolkit.Maui.Core;
+//using CommunityToolkit.Maui.Alerts;
 using productoApp.Models;
 using System.Collections.ObjectModel;
 
@@ -24,8 +24,8 @@ public partial class ProductoPage : ContentPage
     }
     private async void OnClickNuevoProducto(object sender, EventArgs e)
     {
-        var toast = CommunityToolkit.Maui.Alerts.Toast.Make("Click en nuevo producto", ToastDuration.Short, 14);
-        await toast.Show();
+        //var toast = CommunityToolkit.Maui.Alerts.Toast.Make("Click en nuevo producto", ToastDuration.Short, 14);
+        //await toast.Show();
         await Navigation.PushAsync(new NuevoProducto());
 
     }
